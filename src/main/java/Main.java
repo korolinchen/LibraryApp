48fc388e2d2fad01e5db4ab34d0bd1eb110f6854
karolinchen-parmas <karolina.parmas@protonmail.ch>
2023-08-03 17:29:14 +0300

    LibraryApp August 2023

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+import javax.swing.*;
+>>>>>>> 87c7ce7 (LibraryApp August 2023)
 import java.util.List;
 
 public class Main {
@@ -19,12 +23,40 @@ public class Main {
                 new BookOfLibrary("Pan's Labyrinth", "del Toro", "Guillermo", "2019"),
                 new BookOfLibrary("Pan's Labyrinth", "del Toro", "Guillermo", "2019"),
                 new BookOfLibrary("Hellraiser", "Barker", "Clive", "2006"),
+<<<<<<< HEAD
                 new BookOfLibrary("Hellraise", "Barker", "Clive", "2006")
+=======
+                new BookOfLibrary("Hellraise", "Barker", "Clive", "2006"),
+                new BookOfLibrary("Hellraiser2", "Barker", "Clive", "2009"),
+                new BookOfLibrary("Hellraise2", "Barker", "Clive", "2009")
+>>>>>>> 87c7ce7 (LibraryApp August 2023)
         );
         System.out.println("The books you may borrow at our library right now are: \n");
         this.showAllBooksInLibraryNow(listOfBooksInLibrary);
 
+<<<<<<< HEAD
 
+=======
+        String userChoice = "";
+        while (!userChoice.equals(" ")) {
+
+            userChoice = JOptionPane.showInputDialog(
+                    new StringBuilder()
+                            .append("Choose an action please")
+                            .append("\n1. Register new reader")
+                            .append("\n2. See books list")
+                            .append("\n3. Borrow a book")
+                            .append("\n4. Return a book")
+                            .append("\n5. Review reader's info")
+                            .append("\n6. Finish work")
+                            .toString()
+            );
+            switch (userChoice) {
+                case "1":
+
+            }
+        }
+>>>>>>> 87c7ce7 (LibraryApp August 2023)
     }
     public void showAllBooksInLibraryNow(List<BookOfLibrary> theListOfChoice) {
         for (BookOfLibrary currentItem : theListOfChoice) { //on right side the name of the list, on the left type of the item
